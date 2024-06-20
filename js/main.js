bd318e5f8990e32caee9560ae5eb2bc175222bcd
Riya Zavi <54096098+riyaelizabeth@users.noreply.github.com>
2024-06-20 10:40:52 +0530

    move firebase outside

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+// Import the q you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD25Fn5CLlKYPvEB_dZ3tHA3SQcYQQoFiw",
+  authDomain: "johnandcrystal-wedding.firebaseapp.com",
+  projectId: "johnandcrystal-wedding",
+  storageBucket: "johnandcrystal-wedding.appspot.com",
+  messagingSenderId: "118930812421",
+  appId: "1:118930812421:web:7da67b04274875fb55766c",
+  measurementId: "G-7YFKLZ054R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 (function ($) {
   "use strict";
 
